eeb7fbc59bb30531c4837f932c057d95f6132223
Ambika ghadlinge <mail2ghadlinge@gmail.com>
2025-11-16 12:37:32 -0500

    Fixed all the code review comments

--- helloworld.java
+++ helloworld.java
@@ -1,14 +1,11 @@
 class HelloWorld{
-    public  static  void Main(string args[]){
-
+    public  static  void Main(string args[])
+    {
         System.Out.Println("Hello world!!!");
-
     }
 
-    public stating void PrintName(string name){
-
+    public static void PrintName(string name)
+    {
         System.Out.PrintLn("My name is "+ name);
-
     }
-
 }
